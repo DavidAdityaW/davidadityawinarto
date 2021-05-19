1bf54cff07297eb618d80c54c72564d1407c224b
DavidAdityaW <david.10118071@mahasiswa.unikom.ac.id>
2021-05-19 12:39:37 +0700

    splashscreen + walkthrough viewpager2 (david 10118071)

---
+++ app/src/main/java/com/example/davidadityawinarto/Model/WalkthroughModel.java
@@ -0,0 +1,33 @@
+package com.example.davidadityawinarto.Model;
+
+public class WalkthroughModel {
+
+    private int image;
+    private String title;
+    private String subtitle;
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+}
