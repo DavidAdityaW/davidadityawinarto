9383338963a396da36cf76c678c8bf8bba9175bb
DavidAdityaW <david.10118071@mahasiswa.unikom.ac.id>
2021-05-22 10:57:38 +0700

    tampilan profile linkfy + custom dialog (david 10118071)

--- app/src/main/java/com/example/davidadityawinarto/Fragment/ProfileFragment.java
+++ app/src/main/java/com/example/davidadityawinarto/Fragment/ProfileFragment.java
@@ -1,12 +1,18 @@
 package com.example.davidadityawinarto.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Spannable;
+import android.text.util.Linkify;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.davidadityawinarto.R;
 
@@ -57,10 +63,100 @@ public class ProfileFragment extends Fragment {
         }
     }
 
+    ImageView ivFacebook, ivInstagram, ivTwitter, ivWhatsapp, ivGmail, ivMap;
+    TextView tvPhone, tvAbout;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View root = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        ivFacebook = root.findViewById(R.id.iv_facebook);
+        ivInstagram = root.findViewById(R.id.iv_instagram);
+        ivTwitter = root.findViewById(R.id.iv_twitter);
+        ivWhatsapp = root.findViewById(R.id.iv_whatsapp);
+        ivGmail = root.findViewById(R.id.iv_gmail);
+        ivMap = root.findViewById(R.id.iv_map);
+        tvPhone = root.findViewById(R.id.tv_phone);
+        tvAbout = root.findViewById(R.id.tv_about);
+
+        // facebook
+        ivFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent fb = new Intent();
+                fb.setAction(Intent.ACTION_VIEW);
+                fb.addCategory(Intent.CATEGORY_BROWSABLE);
+                fb.setData(Uri.parse("https://www.facebook.com"));
+                startActivity(fb);
+            }
+        });
+
+        // instagram
+        ivInstagram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ig = new Intent();
+                ig.setAction(Intent.ACTION_VIEW);
+                ig.addCategory(Intent.CATEGORY_BROWSABLE);
+                ig.setData(Uri.parse("https://www.facebook.com"));
+                startActivity(ig);
+            }
+        });
+
+        // twitter
+        ivTwitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent tw = new Intent();
+                tw.setAction(Intent.ACTION_VIEW);
+                tw.addCategory(Intent.CATEGORY_BROWSABLE);
+                tw.setData(Uri.parse("https://www.facebook.com"));
+                startActivity(tw);
+            }
+        });
+
+        // whatsapp
+        ivWhatsapp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent wa = new Intent();
+                wa.setAction(Intent.ACTION_VIEW);
+                wa.addCategory(Intent.CATEGORY_BROWSABLE);
+                wa.setData(Uri.parse("https://www.facebook.com"));
+                startActivity(wa);
+            }
+        });
+
+        // gmail
+        ivGmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gm = new Intent();
+                gm.setAction(Intent.ACTION_VIEW);
+                gm.addCategory(Intent.CATEGORY_BROWSABLE);
+                gm.setData(Uri.parse("https://www.facebook.com"));
+                startActivity(gm);
+            }
+        });
+
+        // map
+        ivMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent map = new Intent();
+                map.setAction(Intent.ACTION_VIEW);
+                map.addCategory(Intent.CATEGORY_BROWSABLE);
+                map.setData(Uri.parse("https://www.facebook.com"));
+                startActivity(map);
+            }
+        });
+
+        // phone
+        tvPhone.setText("083121153350");
+        Linkify.addLinks(tvPhone, Linkify.PHONE_NUMBERS);
+
+        return root;
     }
 }
